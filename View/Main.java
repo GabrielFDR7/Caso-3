69ea19ab287d2262fe43fd765b585f02274a2094
sfrancop <s.francop@uniandes.edu.co>
2023-11-06 13:01:36 -0500

    arreglo de  inputs

--- View/Main.java
+++ View/Main.java
@@ -8,56 +8,71 @@ public class Main
     {
         
         System.out.println("Prototipo de miado de bitcoin");
+
+        String algoritmo;
+        String cadena;
+        int ceros;
+        int threads;
         
         boolean cont = true;
         Scanner sc = new Scanner(System.in);
 
         while(cont)
         {
-            System.out.println("Ingrese el algoritmo criptografico de hash");
-            System.out.println("1. SHA256");
-            System.out.println("2. SHA512");
-            int algoritmo = sc.nextInt();
-            if (algoritmo != 0 || algoritmo != 1)
-            {
-                System.out.println("Elección no valida");
+
+            System.out.print("Ingrese el algoritmo criptografico de hash (1. SHA256, 2. SHA512): ");
+            int eleccion = sc.nextInt();
+            if (eleccion != 1 && eleccion != 2){
+                System.out.println("Elección de algoritmo no valida.");
                 cont = false;
             }
-        }
-        
-        cont = true;
-        
-        while (cont)
-        {
-            System.out.println("Ingrese la cadena de la transacción: ");
-            String cadena = sc.nextLine();
-        }
-        
-        while (cont)
-        {
-            System.out.println("Ingrese el numero de 0s iniciales");
-            int ceros = sc.nextInt();
-            int[] arr = {20, 24, 28, 32, 36};
-            boolean encontro = false;
-            for (int i: arr){if (i == ceros){encontro = true;}}
-            if (encontro == false)
-            {   
-                System.out.println("Ingrese un valor valido");
-                cont = false;
+            if (eleccion == 1){
+                algoritmo = "SHA256";
             }
-        }
-        cont = true;
-        
-        while (cont)
-        {
-            System.out.println("Ingrese el numero de threads para la busqueda");
-            int threads = sc.nextInt();
-            if (threads > 0)
-            {
-                System.out.println("Ingrese un valor valido");
-                cont = false;
+            else if (eleccion == 2){
+                algoritmo = "SHA512";
             }
+
+            if (cont){
+                System.out.print("Ingrese la cadena de la transacción: ");
+                cadena = sc.next();
+            }
+
+            if (cont){
+                System.out.print("Ingrese el numero de 0s iniciales: ");
+                ceros = sc.nextInt();
+                int[] arr = {20, 24, 28, 32, 36};
+                boolean encontro = false;
+                for (int i: arr){if (i == ceros){encontro = true;}}
+                if (encontro == false)
+                {   
+                    System.out.println("Cantidad de 0s incorrecta.");
+                    cont = false;
+                }
+            }
+        
+            if (cont){
+                System.out.print("Ingrese el numero de threads para la busqueda (1 o 2): ");
+                threads = sc.nextInt();
+                if (threads <= 0 || threads > 2){
+                    System.out.println("Ingrese un número de threads valido");
+                    cont = false;
+                }
+            }
+
+            if (cont){
+                System.out.print("¿Desea terminar el programa después de esta ejecución? (S/N): ");
+                String respuesta = sc.next();
+                if (respuesta.equalsIgnoreCase("S")){
+                    cont = false;
+                }
+            }
+
         }
+        
+        
+
     }
+
 }
 
